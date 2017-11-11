cf48f4473c70d4c0feb3c78bcc26c15608fcbcd2
Marcin Miotk <mami@snmill.com>
2017-11-11 19:45:40 +0100

    Accounts configuration

---
+++ src/main/java/app/freecalls/config/AccountsConfiguration.java
@@ -0,0 +1,79 @@
+package app.freecalls.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+import java.util.StringJoiner;
+
+@Configuration
+@ConfigurationProperties(prefix = "accounts")
+class AccountsConfiguration {
+
+    private static final Logger logger = LoggerFactory.getLogger(AccountsConfiguration.class);
+
+    private Account pinging;
+    private Account firstLeg;
+    private Account secondLeg;
+
+    @PostConstruct
+    void print() {
+        StringJoiner out = new StringJoiner(";");
+        out.add("Accounts configuration");
+        out.add("Pinging");
+        out.add(pinging.username);
+        out.add("First leg");
+        out.add(firstLeg.username);
+        out.add("Second leg");
+        out.add(secondLeg.username);
+        logger.info(out.toString());
+    }
+
+    public Account getPinging() {
+        return pinging;
+    }
+
+    public void setPinging(Account pinging) {
+        this.pinging = pinging;
+    }
+
+    public Account getFirstLeg() {
+        return firstLeg;
+    }
+
+    public void setFirstLeg(Account firstLeg) {
+        this.firstLeg = firstLeg;
+    }
+
+    public Account getSecondLeg() {
+        return secondLeg;
+    }
+
+    public void setSecondLeg(Account secondLeg) {
+        this.secondLeg = secondLeg;
+    }
+
+    static class Account {
+
+        private String username;
+        private String password;
+
+        public String getUsername() {
+            return username;
+        }
+
+        public void setUsername(String username) {
+            this.username = username;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+    }
+}
---
+++ src/test/java/app/freecalls/config/AccountsConfigurationIntegrationTest.java
@@ -0,0 +1,49 @@
+package app.freecalls.config;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AccountsConfigurationIntegrationTest {
+
+    @Autowired
+    AccountsConfiguration accountsConfiguration;
+
+    @Test
+    public void account_pinging_with_username() {
+        assertEquals("pinging-test-user", accountsConfiguration.getPinging().getUsername());
+    }
+
+    @Test
+    public void account_pinging_with_password() {
+        assertEquals("pinging-test-user-password", accountsConfiguration.getPinging().getPassword());
+    }
+
+    @Test
+    public void account_firstLeg_with_username() {
+        assertEquals("first-test-user", accountsConfiguration.getFirstLeg().getUsername());
+    }
+
+    @Test
+    public void account_firstLeg_with_password() {
+        assertEquals("first-test-user-password", accountsConfiguration.getFirstLeg().getPassword());
+    }
+
+    @Test
+    public void account_secondLeg_with_username() {
+        assertEquals("second-test-user", accountsConfiguration.getSecondLeg().getUsername());
+    }
+
+    @Test
+    public void account_secondLeg_with_password() {
+        assertEquals("second-test-user-password", accountsConfiguration.getSecondLeg().getPassword());
+    }
+
+
+}
