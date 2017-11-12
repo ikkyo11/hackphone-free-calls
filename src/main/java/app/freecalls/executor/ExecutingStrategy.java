a088dcdedc1c8687ffdaefc08f0869656a766a11
Marcin Miotk <mami@snmill.com>
2017-11-12 13:49:01 +0100

    refactoring

--- src/main/java/app/freecalls/executor/ExecutingStrategy.java
+++ src/main/java/app/freecalls/executor/ExecutingStrategy.java
@@ -1,13 +1,11 @@
 package app.freecalls.executor;
 
-import app.freecalls.orders.OrderExecutor;
 import app.freecalls.orders.ThrottableOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
 
 @Component
 class ExecutingStrategy {
@@ -22,9 +20,10 @@ class ExecutingStrategy {
 
     @Scheduled(initialDelayString = "${executing.strategy.initialDelay:5000}", fixedRateString = "${executing.strategy.rate:5000}")
     void scheduled() {
-        Optional<OrderExecutor.OrderExecutorDriver> driver = orders.executeOne(new OrderExecutorScenario());
-        driver.ifPresentOrElse(drv -> logger.info("Driver {}", drv), ()-> {
-            logger.debug("No order to execute.");
-        });
+        orders.executeOne(new OrderExecutorScenario())
+                .ifPresentOrElse(
+                        drv -> logger.info("Driver {}", drv),
+                        ()-> logger.debug("No order to execute.")
+                );
     }
 }
