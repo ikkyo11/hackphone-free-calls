1b7ef57b00756997e7f6fb28780c88fae35701a2
Marcin Miotk <mami@snmill.com>
2017-11-12 08:48:31 +0100

    Orders container

--- src/main/java/app/freecalls/pinging/OrderDefault.java
+++ src/main/java/app/freecalls/pinging/OrderDefault.java
@@ -4,13 +4,68 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
+import javax.sdp.Phone;
+import java.util.LinkedList;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
+
 @Component
 public class OrderDefault implements Order {
 
     private static final Logger logger = LoggerFactory.getLogger(OrderDefault.class);
 
+    final LinkedList<PhoneNumber> orders = new LinkedList<>();
+
     @Override
     public void callMe(PhoneNumber phoneNumber) {
         logger.info("I will call {} to get phone number of friend.", phoneNumber);
+        orders.offer(phoneNumber);
     }
+
+    Optional<Session> getPendingSession() {
+        return Optional.ofNullable(pending.get());
+    }
+
+    private final AtomicReference<Session> pending = new AtomicReference<>();
+
+    void executeOne(OrderExecutor executor) {
+        PhoneNumber number = orders.poll();
+        pending.set(new Session(number));
+        OrderExecutorDriver driver = new OrderExecutorDriver() {
+            @Override
+            public void finish() {
+                pending.set(null);
+            }
+
+            @Override
+            public void rollback() {
+                orders.offer(number);
+            }
+        };
+        executor.onStarted(driver);
+    }
+
+
+    int countOrders() {
+        return orders.size();
+    }
+
+
+    public interface OrderExecutorDriver {
+        void finish();
+        void rollback();
+    }
+
+    public interface OrderExecutor {
+        void onStarted(OrderExecutorDriver driver);
+    }
+
+    static class Session {
+        private final PhoneNumber by;
+
+        public Session(PhoneNumber by) {
+            this.by = by;
+        }
+    }
+
 }
---
+++ src/test/java/app/freecalls/pinging/OrderDefaultTest.java
@@ -0,0 +1,81 @@
+package app.freecalls.pinging;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OrderDefaultTest {
+
+    OrderDefault sut;
+
+    @Before
+    public void before() {
+        sut = new OrderDefault();
+    }
+
+    @Test
+    public void offered_3_numbers_so_3_orders_in_sut() {
+        sut.callMe(new PhoneNumber("111555444"));
+        sut.callMe(new PhoneNumber("222555444"));
+        sut.callMe(new PhoneNumber("333555444"));
+        assertEquals(3L, sut.countOrders());
+    }
+
+    @Test
+    public void no_session_is_in_pending_state_because_no_executor_was_set() {
+        sut.callMe(new PhoneNumber("111555444"));
+        assertEquals(false, sut.getPendingSession().isPresent());
+    }
+
+    @Test
+    public void no_executor_was_set_so_there_are_1_orders() {
+        sut.callMe(new PhoneNumber("111555444"));
+        assertEquals(1L, sut.countOrders());
+    }
+
+    @Test
+    public void one_session_is_in_pending_state_because_executor_was_set() {
+        sut.callMe(new PhoneNumber("111555444"));
+        sut.executeOne(driver -> {
+
+        });
+        assertEquals(true, sut.getPendingSession().isPresent());
+    }
+
+    @Test
+    public void no_sessions_are_in_orders_because_executor_was_set_and_one_session_was_pended() {
+        sut.callMe(new PhoneNumber("111555444"));
+        sut.executeOne(driver -> {
+
+        });
+        assertEquals(0L, sut.countOrders());
+    }
+
+    @Test
+    public void there_is_no_pending_session_after_finished_session() {
+        sut.callMe(new PhoneNumber("111555444"));
+        sut.executeOne(driver -> {
+            driver.finish();
+        });
+        assertEquals(false, sut.getPendingSession().isPresent());
+    }
+
+    @Test
+    public void no_sessions_are_in_orders_aftere_finishing_the_last_one() {
+        sut.callMe(new PhoneNumber("111555444"));
+        sut.executeOne(driver -> {
+            driver.finish();
+        });
+        assertEquals(0L, sut.countOrders());
+    }
+
+    @Test
+    public void executions_with_errors_affects_rollbacking_to_orders() {
+        sut.callMe(new PhoneNumber("111555444"));
+        sut.executeOne(driver -> {
+            driver.rollback();
+        });
+        assertEquals(1L, sut.countOrders());
+    }
+}
